4d2eaa9650bfb638870bbbb0109e31b97dae2371
usherkielvin <usherponce@gmail.com>
2025-08-24 20:16:57 +0800

    "positioned mobile frame to bottom center"

--- src/main/java/cephra/Launcher.java
+++ src/main/java/cephra/Launcher.java
@@ -25,7 +25,14 @@ public final class Launcher {
             monitor.setVisible(true);
 
             cephra.Frame.Phone phone = new cephra.Frame.Phone();
-            phone.setLocationRelativeTo(null);
+            
+            // Get taskbar height (typically 40-50 pixels on Windows)
+            int taskbarHeight = 30;
+            
+            phone.setLocation(
+                screenBounds.x + (screenBounds.width - phone.getWidth()) / 2,  // Center horizontally
+                screenBounds.y + screenBounds.height - phone.getHeight() - taskbarHeight  // Above taskbar
+            );
             phone.setVisible(true);
             phone.toFront();
             phone.requestFocus();
