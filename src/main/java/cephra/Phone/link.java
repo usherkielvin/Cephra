3c122b559304cc8537d89287b69e30bd3e93aee8
usherkielvin <usherponce@gmail.com>
2025-08-24 20:08:10 +0800

    "updated link panel to gif ni dwyane"

--- src/main/java/cephra/Phone/link.java
+++ src/main/java/cephra/Phone/link.java
@@ -29,55 +29,53 @@ public class link extends javax.swing.JPanel {
         charge.setBorderPainted(false);
         charge.setContentAreaFilled(false);
         charge.setFocusPainted(false);
-        add(charge);
-        charge.setBounds(40, 670, 50, 60);
         charge.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 chargeActionPerformed(evt);
             }
         });
+        add(charge);
+        charge.setBounds(40, 670, 50, 60);
 
         historybutton.setBorder(null);
         historybutton.setBorderPainted(false);
         historybutton.setContentAreaFilled(false);
         historybutton.setFocusPainted(false);
-        add(historybutton);
-        historybutton.setBounds(230, 653, 50, 100);
         historybutton.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 historybuttonActionPerformed(evt);
             }
         });
+        add(historybutton);
+        historybutton.setBounds(230, 653, 50, 100);
 
         profilebutton.setBorder(null);
         profilebutton.setBorderPainted(false);
         profilebutton.setContentAreaFilled(false);
         profilebutton.setFocusPainted(false);
-        add(profilebutton);
-        profilebutton.setBounds(290, 660, 80, 70);
         profilebutton.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 profilebuttonActionPerformed(evt);
             }
         });
+        add(profilebutton);
+        profilebutton.setBounds(290, 660, 80, 70);
 
         homebutton.setBorder(null);
         homebutton.setBorderPainted(false);
         homebutton.setContentAreaFilled(false);
         homebutton.setFocusPainted(false);
-        add(homebutton);
-        homebutton.setBounds(170, 680, 50, 40);
         homebutton.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 homebuttonActionPerformed(evt);
             }
         });
+        add(homebutton);
+        homebutton.setBounds(170, 680, 50, 40);
 
-        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/cephra/Photos/LINKING.png"))); // NOI18N
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/cephra/Photos/link_gif.gif"))); // NOI18N
         add(jLabel1);
         jLabel1.setBounds(-15, 0, 398, 750);
-        // Keep background behind interactive components
-        setComponentZOrder(jLabel1, getComponentCount() - 1);
     }// </editor-fold>//GEN-END:initComponents
 
     private void chargeActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_chargeActionPerformed
