60df69a42acdebd0d3dde86b52265a76acdc112d
cwossant <dwayynneeee@gmail.com>
2025-08-25 14:39:33 +0800

    Updated bottom nav bar for home-page, linking-panel, service-offered panel, profile panel

--- src/main/java/cephra/Phone/home.java
+++ src/main/java/cephra/Phone/home.java
@@ -61,55 +61,53 @@ public class home extends javax.swing.JPanel {
         profilebutton.setBorderPainted(false);
         profilebutton.setContentAreaFilled(false);
         profilebutton.setFocusPainted(false);
-        add(profilebutton);
-        profilebutton.setBounds(290, 660, 80, 70);
         profilebutton.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 profilebuttonActionPerformed(evt);
             }
         });
+        add(profilebutton);
+        profilebutton.setBounds(260, 670, 50, 50);
 
         historybutton.setBorder(null);
         historybutton.setBorderPainted(false);
         historybutton.setContentAreaFilled(false);
         historybutton.setFocusPainted(false);
-        add(historybutton);
-        historybutton.setBounds(230, 653, 50, 100);
         historybutton.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 historybuttonActionPerformed(evt);
             }
         });
+        add(historybutton);
+        historybutton.setBounds(200, 680, 50, 40);
 
         charge.setBorder(null);
         charge.setBorderPainted(false);
         charge.setContentAreaFilled(false);
         charge.setFocusPainted(false);
-        add(charge);
-        charge.setBounds(40, 690, 50, 40);
         charge.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 chargeActionPerformed(evt);
             }
         });
+        add(charge);
+        charge.setBounds(30, 680, 50, 40);
 
         linkbutton.setBorder(null);
         linkbutton.setBorderPainted(false);
         linkbutton.setContentAreaFilled(false);
         linkbutton.setFocusPainted(false);
-        add(linkbutton);
-        linkbutton.setBounds(100, 680, 50, 40);
         linkbutton.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 linkbuttonActionPerformed(evt);
             }
         });
+        add(linkbutton);
+        linkbutton.setBounds(90, 680, 50, 40);
 
-        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/cephra/Photos/HOME PAGE.png"))); // NOI18N
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/cephra/Photos/HOME PAGE-updated.png"))); // NOI18N
         add(jLabel1);
-        // Label position will be set in setupLabelPosition() method
-        // Keep background behind interactive components
-        setComponentZOrder(jLabel1, getComponentCount() - 1);
+        jLabel1.setBounds(-15, 0, 398, 750);
     }// </editor-fold>//GEN-END:initComponents
 
     // CUSTOM CODE - DO NOT REMOVE - NetBeans will regenerate form code but this method should be preserved
