55e1508a54d4b9d4a95b72049cdd59156a2a3dd8
usherkielvin <usherponce@gmail.com>
2025-08-26 15:47:11 +0800

    Add Link button to link form and implement action handling for navigation; adjust home button dimensions for improved layout.

--- src/main/java/cephra/Phone/link.java
+++ src/main/java/cephra/Phone/link.java
@@ -16,6 +16,7 @@ public class link extends javax.swing.JPanel {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        Link = new javax.swing.JButton();
         charge = new javax.swing.JButton();
         historybutton = new javax.swing.JButton();
         profilebutton = new javax.swing.JButton();
@@ -25,6 +26,17 @@ public class link extends javax.swing.JPanel {
         setMaximumSize(new java.awt.Dimension(350, 750));
         setPreferredSize(new java.awt.Dimension(350, 750));
         setLayout(null);
+
+        Link.setBorder(null);
+        Link.setBorderPainted(false);
+        Link.setContentAreaFilled(false);
+        Link.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                LinkActionPerformed(evt);
+            }
+        });
+        add(Link);
+        Link.setBounds(50, 560, 240, 50);
 
         charge.setBorder(null);
         charge.setBorderPainted(false);
@@ -72,7 +84,7 @@ public class link extends javax.swing.JPanel {
             }
         });
         add(homebutton);
-        homebutton.setBounds(170, 680, 50, 40);
+        homebutton.setBounds(150, 680, 40, 40);
 
         jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/cephra/Photos/ConnectCar.gif"))); // NOI18N
         add(jLabel1);
@@ -147,8 +159,24 @@ public class link extends javax.swing.JPanel {
         });
     }//GEN-LAST:event_homebuttonActionPerformed
 
+    private void LinkActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_LinkActionPerformed
+          SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                java.awt.Window[] windows = java.awt.Window.getWindows();
+                for (java.awt.Window window : windows) {
+                    if (window instanceof cephra.Frame.Phone) {
+                        cephra.Frame.Phone phoneFrame = (cephra.Frame.Phone) window;
+                        phoneFrame.switchPanel(new cephra.Phone.LinkedCar());
+                        break;
+                    }
+                }
+            }
+        });
+    }//GEN-LAST:event_LinkActionPerformed
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton Link;
     private javax.swing.JButton charge;
     private javax.swing.JButton historybutton;
     private javax.swing.JButton homebutton;
