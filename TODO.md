5ccb315555de962a09aed73cd071973fc892d31c
dwayynnee <dwayynnee@gmail.com>
2025-09-05 19:56:58 +0800

    mobile api

---
+++ TODO.md
@@ -0,0 +1,7 @@
+# TODO: Fix href links in dashboard.php
+
+- [x] Edit Twitter href: Change href="www.x.com" to href="https://www.x.com/@Cephra"
+- [x] Edit Facebook href: Change href="www.facebook.com" to href="https://www.facebook.com/CephraCorp"
+- [x] Edit WWW href: Change href="www.gmail.com" to href="https://www.cephra.com" (placeholder)
+- [x] Edit Email href: Change href="www.gmail.com" to href="mailto:admin@cephra.com"
+- [ ] Test the links by opening the page in a browser
---
+++ mobileweb/dashboard.php
@@ -0,0 +1,253 @@
+<!DOCTYPE HTML>
+<!--
+	ZeroFour by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>Cephra</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="icon" type="image/png" href="images/logo.png" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+	</head>
+	<body class="homepage is-preload">
+		<div id="page-wrapper">
+
+			<!-- Header -->
+				<div id="header-wrapper">
+					<div class="container">
+
+						<!-- Header -->
+							<header id="header">
+								<div class="inner">
+
+									<!-- Logo -->
+										<h1><a href="index.html" id="logo">Cephra</a></h1>
+
+									<!-- Nav -->
+										<nav id="nav">
+											<ul>
+												<li class="current_page_item"><a href="index.html">Home</a></li>
+												<li><a href="left-sidebar.html">Link</a></li>
+												<li><a href="right-sidebar.html">History</a></li>
+												<li><a href="no-sidebar.html">Profile</a></li>
+											</ul>
+										</nav>
+
+								</div>
+							</header>
+
+						<!-- Banner -->
+							<div id="banner">
+								<h2> 
+								<a href="#" class="image featured"><img src="images/ads.png" alt="" /></a>
+								</h2>
+								<p>Every tap starts your power. With Cephra, charging isn’t just energy it’s freedom. </p>
+								<a href="#" id="start-charging" class="button large">Start Charging</a>
+							</div>
+
+					</div>
+				</div>
+
+			<!-- Main Wrapper -->
+				<div id="main-wrapper">
+					<div class="wrapper style1">
+						<div class="inner">
+
+							
+
+							<!-- Feature 2 -->
+								<section class="container box feature2">
+									<div class="row">
+										<div class="col-6 col-12-medium">
+											<section>
+												<header class="major">
+													<h2>And this is a subheading</h2>
+													<p>It’s important but clearly not *that* important</p>
+												</header>
+												<p>Phasellus quam turpis, feugiat sit amet ornare in, hendrerit in lectus.
+												Praesent semper mod quis eget mi. Etiam eu ante risus. Aliquam erat volutpat.
+												Aliquam luctus et mattis lectus sit amet pulvinar. Nam turpis nisi
+												consequat etiam.</p>
+												<footer>
+													<a href="#" class="button medium icon solid fa-arrow-circle-right">Let's do this</a>
+												</footer>
+											</section>
+										</div>
+										<div class="col-6 col-12-medium">
+											<section>
+												<header class="major">
+													<h2>This is also a subheading</h2>
+													<p>And is as unimportant as the other one</p>
+												</header>
+												<p>Phasellus quam turpis, feugiat sit amet ornare in, hendrerit in lectus.
+												Praesent semper mod quis eget mi. Etiam eu ante risus. Aliquam erat volutpat.
+												Aliquam luctus et mattis lectus sit amet pulvinar. Nam turpis nisi
+												consequat etiam.</p>
+												<footer>
+													<a href="#" class="button medium alt icon solid fa-info-circle">Wait, what?</a>
+												</footer>
+											</section>
+										</div>
+									</div>
+								</section>
+
+							</div>
+					</div>
+					<div class="wrapper style3">
+						<div class="inner">
+							<div class="container">
+								<div class="row">
+									<div class="col-8 col-12-medium">
+
+										<!-- Article list -->
+											<section class="box article-list">
+												<h2 class="icon fa-file-alt">Recent Posts</h2>
+
+												<!-- Excerpt -->
+													<article class="box excerpt">
+														<a href="#" class="image left"><img src="images/pic04.jpg" alt="" /></a>
+														<div>
+															<header>
+																<span class="date">July 24</span>
+																<h3><a href="#">Repairing a hyperspace window</a></h3>
+															</header>
+															<p>Phasellus quam turpis, feugiat sit amet ornare in, hendrerit in lectus
+															semper mod quisturpis nisi consequat etiam lorem. Phasellus quam turpis,
+															feugiat et sit amet ornare in, hendrerit in lectus semper mod quis eget mi dolore.</p>
+														</div>
+													</article>
+
+												<!-- Excerpt -->
+													<article class="box excerpt">
+														<a href="#" class="image left"><img src="images/pic05.jpg" alt="" /></a>
+														<div>
+															<header>
+																<span class="date">July 18</span>
+																<h3><a href="#">Adventuring with a knee injury</a></h3>
+															</header>
+															<p>Phasellus quam turpis, feugiat sit amet ornare in, hendrerit in lectus
+															semper mod quisturpis nisi consequat etiam lorem. Phasellus quam turpis,
+															feugiat et sit amet ornare in, hendrerit in lectus semper mod quis eget mi dolore.</p>
+														</div>
+													</article>
+
+												<!-- Excerpt -->
+													<article class="box excerpt">
+														<a href="#" class="image left"><img src="images/pic06.jpg" alt="" /></a>
+														<div>
+															<header>
+																<span class="date">July 14</span>
+																<h3><a href="#">Preparing for Y2K38</a></h3>
+															</header>
+															<p>Phasellus quam turpis, feugiat sit amet ornare in, hendrerit in lectus
+															semper mod quisturpis nisi consequat etiam lorem. Phasellus quam turpis,
+															feugiat et sit amet ornare in, hendrerit in lectus semper mod quis eget mi dolore.</p>
+														</div>
+													</article>
+
+											</section>
+									</div>
+									<div class="col-4 col-12-medium">
+
+										<!-- Spotlight -->
+											<section class="box spotlight">
+												<h2 class="icon fa-file-alt">Spotlight</h2>
+												<article>
+													<a href="#" class="image featured"><img src="images/pic07.jpg" alt=""></a>
+													<header>
+														<h3><a href="#">Neural Implants</a></h3>
+														<p>The pros and cons. Mostly cons.</p>
+													</header>
+													<p>Phasellus quam turpis, feugiat sit amet ornare in, hendrerit in lectus semper mod
+													quisturpis nisi consequat ornare in, hendrerit in lectus semper mod quis eget mi quat etiam
+													lorem. Phasellus quam turpis, feugiat sed et lorem ipsum dolor consequat dolor feugiat sed
+													et tempus consequat etiam.</p>
+													<p>Lorem ipsum dolor quam turpis, feugiat sit amet ornare in, hendrerit in lectus semper
+													mod quisturpis nisi consequat etiam lorem sed amet quam turpis.</p>
+													<footer>
+														<a href="#" class="button alt icon solid fa-file-alt">Continue Reading</a>
+													</footer>
+												</article>
+											</section>
+
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+
+			<!-- Footer Wrapper -->
+				<div id="footer-wrapper">
+					<footer id="footer" class="container">
+						<div class="row">
+
+
+								<!-- About -->
+									<section>
+										<h2><strong>Cephra</strong> by CephraCorp</h2>
+										<p>Hi! This is <strong>Cephra</strong>, a free, fully responsive site for charging your
+										electric vehicle. its built on HTML5 and CSS3 </a>.
+										</p>
+									</section>
+
+								<!-- Contact -->
+									<section>
+										<h2>Get in touch</h2>
+										<div>
+											<div class="row">
+												<div class="col-6 col-12-small">
+													<dl class="contact">
+														<dt>Twitter</dt>
+														<dd><a href="https://www.x.com/@Cephra">@Cephra</a></dd>
+														<dt>Facebook</dt>
+														<dd><a href="https://www.facebook.com/CephraCorp">facebook.com/CephraCorp</a></dd>
+														<dt>WWW</dt>
+														<dd><a href="https://www.cephra.com">CephraCorp.tld</a></dd>
+														<dt>Email</dt>
+														<dd><a href="mailto:admin@cephra.com">admin@Cephra.gmail.com</a></dd>
+													</dl>
+												</div>
+												<div class="col-6 col-12-small">
+													<dl class="contact">
+														<dt>Address</dt>
+														<dd>
+															Coral Way St.<br />
+															Moa Complex, Pasay City<br />
+															PH
+														</dd>
+														<dt>Phone</dt>
+														<dd>(000) 000-0000</dd>
+													</dl>
+												</div>
+											</div>
+										</div>
+									</section>
+
+							</div>
+							<div class="col-12">
+								<div id="copyright">
+									<ul class="menu">
+										<li>&copy; Cephra. All rights reserved</li><li>Design: <a href="http://html5up.net">Cephra Designer</a></li>
+									</ul>
+								</div>
+							</div>
+						</div>
+					</footer>
+				</div>
+
+		</div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.dropotron.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
