f479a5902d59e8afe5efb3e914eb29c69a62438a
usherkielvin <usherponce@gmail.com>
2025-09-16 11:07:33 +0800

    Update Transition.java to change the background image from Transis.gif to Coolintro.gif for improved visual presentation.

--- src/main/java/cephra/Phone/Utilities/Transition.java
+++ src/main/java/cephra/Phone/Utilities/Transition.java
@@ -91,7 +91,7 @@ public class Transition extends javax.swing.JPanel {
         add(jLabel1);
         jLabel1.setBounds(0, 0, 370, 750);
 
-        jLabel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/cephra/Cephra Images/Transis.gif"))); // NOI18N
+        jLabel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/cephra/Cephra Images/Coolintro.gif"))); // NOI18N
         add(jLabel2);
         jLabel2.setBounds(0, 0, 370, 750);
     }// </editor-fold>//GEN-END:initComponents
